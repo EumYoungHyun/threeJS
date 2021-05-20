6c4436713d10528f45b833ad6f9d1587f3ec6a3e
Eum <opwer032@naver.com>
2021-05-20 10:03:29 +0900

    feat: Complete first three js

--- 01_GettingStart/three.js
+++ 01_GettingStart/three.js
@@ -8,3 +8,20 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x663300 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+animate();
