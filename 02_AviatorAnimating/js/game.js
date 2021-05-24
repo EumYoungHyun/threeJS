6cef436922d13c5c98b1219560d8e998b608225a
Eum <opwer032@naver.com>
2021-05-24 09:38:55 +0900

    feat: Complete createScene

--- 02_AviatorAnimating/js/game.js
+++ 02_AviatorAnimating/js/game.js
@@ -1,48 +1,56 @@
-Colors = {
-  red: 0xf25346,
-  white: 0xd8d0d1,
-  brown: 0x59332e,
-  pink: 0xf5986e,
-  brownDark: 0x23190f,
-  blue: 0x68c3c0,
+const Colors = {
+    red: 0xf25346,
+    white: 0xd8d0d1,
+    brown: 0x59332e,
+    pink: 0xf5986e,
+    brownDark: 0x23190f,
+    blue: 0x68c3c0,
 };
 
 window.addEventListener("load", init, false);
 
 function init() {
-  // set up the scene, the camera and the renderer
-  createScene();
+    // set up the scene, the camera and the renderer
+    createScene();
 }
 
 let scene, camera, WIDTH, HEIGHT;
 function createScene() {
-  WIDTH = window.innerWidth;
-  HEIGHT = window.innerHeight;
-  scene = new THREE.Scene();
+    WIDTH = window.innerWidth;
+    HEIGHT = window.innerHeight;
 
-  scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+    scene = new THREE.Scene();
+    aspectRatio = WIDTH / HEIGHT;
+    fieldOfView = 60;
+    nearPlane = 1;
+    farPlane = 10000;
 
-  fieldOfView = 60;
-  aspectRatio = WIDTH / HEIGHT;
-  nearPlane = 1;
-  farPlane = 10000;
-  camera = new THREE.PerspectiveCamera(
-    fieldOfView,
-    aspectRatio,
-    nearPlane,
-    farPlane
-  );
-  camera.position.x = 0;
-  camera.position.y = 100;
-  camera.position.z = 200;
+    scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
 
-  renderer = new THREE.WebGLRenderer({
-    alpha: true,
-    antialias: true,
-  });
-  renderer.setSize(WIDTH, HEIGHT);
-  renderer.shadowMap.enabled = true;
+    camera = new THREE.PerspectiveCamera(
+        fieldOfView,
+        aspectRatio,
+        nearPlane,
+        farPlane
+    );
+    camera.position.x = 0;
+    camera.position.y = 100;
+    camera.position.z = 200;
 
-  container = document.querySelector("#world");
-  container.appendChild(renderer.domElement);
+    renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+    renderer.setSize(WIDTH, HEIGHT);
+    renderer.shadowMap.enabled = true;
+
+    container = document.querySelector("#world");
+    container.appendChild(renderer.domElement);
+
+    window.addEventListener("resize", handleWindowResize, false);
+}
+
+function handleWindowResize() {
+    WIDTH = window.innerWidth;
+    HEIGHT = window.innerHeight;
+    renderer.setSize(WIDTH, HEIGHT);
+    camera.aspect = WIDTH / HEIGHT;
+    camera.updateProjectionMatrix();
 }
