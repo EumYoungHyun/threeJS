a31c7c79d6dc6b6dd51bb70e1a50793d231bd589
EumYoungHyun <opwer032@naver.com>
2021-07-11 17:51:11 +0900

    feat: Add profile

--- 03_PortfolioWebTutorial/main.js
+++ 03_PortfolioWebTutorial/main.js
@@ -1,6 +1,7 @@
-import "./style.css";
-
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import "./style.css";
 
 const scene = new THREE.Scene();
 
@@ -20,4 +21,69 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render(scene, camera);
+// Make Torus
+
+// geometry: the {x, y, z} points that makeup a shape
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+// material: the wrapping paper for an object
+const material = new THREE.MeshStandardMaterial({
+  color: 0xff6347,
+});
+// mesh: geometry + material
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.OctahedronGeometry(0.25);
+  const meterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+scene.background = spaceTexture;
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+// Avartar
+
+const myTexture = new THREE.TextureLoader().load("Eumericano.jpg");
+
+const me = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({ map: myTexture })
+);
+
+scene.add(me);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.007;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
