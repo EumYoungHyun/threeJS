7f35211257135232f0f9b3a8ef7478022e305d8a
EumYoungHyun <opwer032@naver.com>
2021-07-25 23:52:19 +0900

    feat: Set scene, camera, renderer

--- 04_reflectOnRealPlace/main.js
+++ 04_reflectOnRealPlace/main.js
@@ -1,2 +1,19 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+let scene, camera, renderer;
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    1,
+    5000
+  );
+  camera.position.set(0, 400, 1000);
+
+  renderer = new THREE.WebGL1Renderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+}
