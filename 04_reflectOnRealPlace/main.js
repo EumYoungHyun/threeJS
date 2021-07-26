5b6368a240225e3680400050e186dc051630ca75
EumYoungHyun <opwer032@naver.com>
2021-07-26 20:53:36 +0900

    feat; Complete project

--- 04_reflectOnRealPlace/main.js
+++ 04_reflectOnRealPlace/main.js
@@ -1,7 +1,8 @@
 import * as THREE from "three";
+import { CubeCamera } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-let scene, camera, renderer;
+let scene, camera, renderer, sphereCamera;
 function init() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
@@ -20,6 +21,24 @@ function init() {
   let controls = new OrbitControls(camera, renderer.domElement);
   controls.enableZoom = false;
 
+  const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(128, {
+    format: THREE.RGBFormat,
+    generateMipmaps: true,
+    minFilter: THREE.LinearMipmapLinearFilter,
+  });
+
+  sphereCamera = new CubeCamera(1, 1000, cubeRenderTarget);
+  sphereCamera.position.set(0, 100, 0);
+  scene.add(sphereCamera);
+
+  let sphereMaterial = new THREE.MeshBasicMaterial({
+    envMap: sphereCamera.renderTarget,
+  });
+  let sphereGeo = new THREE.SphereGeometry(350, 50, 70);
+  let sphere = new THREE.Mesh(sphereGeo, sphereMaterial);
+  sphere.position.set(0, 100, 0);
+  scene.add(sphere);
+
   const urls = [
     "images/posx.jpg",
     "images/negx.jpg",
@@ -34,6 +53,7 @@ function init() {
 
   function render() {
     renderer.render(scene, camera);
+    sphereCamera.update(renderer, scene);
     requestAnimationFrame(render);
   }
 
